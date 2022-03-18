f90ec4255e2190998f18d4e0c634a70fd227599c
Pedro Nascimento <pedroalex_infotec@yahoo.com.br>
2022-03-18 10:19:16 -0300

    Aula 17/03/2022

---
+++ aula-form/cadastro.php
@@ -0,0 +1,9 @@
+<?php
+  echo "<h2>Dados do Cliente</h2>";
+  echo "<b>Nome: </b>".$_GET["nome"]."<br>";
+  echo "<b>Idade: </b>".$_REQUEST["idade"]."<br>";
+  echo "<b>Sexo: </b>".$_REQUEST["sexo"]."<br>";
+  echo "<b>e-mail: </b>".$_REQUEST["email"]."<br>";
+  echo "<b>Senha: </b>".$_REQUEST["senha"]."<br>";
+  echo "<b>Admissão: </b>".$_REQUEST["admissao"]."<br>";
+?>
